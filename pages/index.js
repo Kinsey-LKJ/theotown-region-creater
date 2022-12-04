cdbd16940b5eb13c7d4f26651f18cb68fcea5313
kinsey-lkj <kinsey@lkj.design>
2022-12-04 17:21:23 +0800

    修改首页的版本号

--- pages/index.js
+++ pages/index.js
@@ -424,7 +424,7 @@ export default function Home() {
       <Container className={styles.container}>
         <h1>
           TheoTown <br />
-          地图创建工具 <span className={styles.betaSign}>1.1.3</span>
+          地图创建工具 <span className={styles.betaSign}>1.1.4</span>
         </h1>
         <Input
           value={name}
